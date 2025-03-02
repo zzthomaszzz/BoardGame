d152b7da9481f6f63870be03047f9038d3ef6940
Thomas Minh Nguyen <thomasnguyen8503@gmail.com>
2025-03-02 22:50:57 +1300

    Map is done
    Need to make a back-end map system

--- index.js
+++ index.js
@@ -11,10 +11,39 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
+app.use(express.static(__dirname+'/public'));
+
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
 
+//List of all players
+var SOCKET = {};
+
+class Player{
+  constructor(id){
+    this.id = this.id;
+    this.hero = new defaultHero();
+  }
+}
+
+class defaultHero{
+  constructor(){
+    this.health = 100;
+    this.attack = 5;
+    this.armor = 5;
+    this.speed = 2;
+  }
+}
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    socket.id = Math.random();
+    console.log(socket.id);
+    SOCKET[socket.id] = new Player(socket.id);
+
 });
+
+io.on('disconnect', (socket) => {
+  delete SOCKET[socket.id];
+  console.log('deleted player id: ' + socket.id);
+})
